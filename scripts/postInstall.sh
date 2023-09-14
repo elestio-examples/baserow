93927b2c73616d575786226e3e2641dc2d5ce507
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-09-14 15:26:11 +0300

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -5,15 +5,5 @@ set -o allexport; source .env; set +o allexport;
 echo "Waiting for software to be ready ..."
 sleep 30s;
 
-target=$(docker-compose port baserow 80)
 
-
-curl http://${target}/api/user/ \
-  -H 'accept: application/json' \
-  -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6' \
-  -H 'cache-control: no-cache' \
-  -H 'content-type: application/json' \
-  -H 'pragma: no-cache' \
-  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36' \
-  --data-raw '{"name":"admin","email":"'${ADMIN_EMAIL}'","password":"'${ADMIN_PASSWORD}'","authenticate":true,"language":"fr"}' \
-  --compressed
+  docker-compose exec -T baserow curl -X POST -H 'Content-Type: application/json' -i http://localhost/api/user/ --data '{ "name": "Admin", "email": "'${ADMIN_EMAIL}'", "password": "'${ADMIN_PASSWORD}'" }'
