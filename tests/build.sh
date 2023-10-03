2bc7d27c0f67fde94284c3a0b27e483757a1373d
Prem Chavhan <pchavhan@logiquad.com>
2023-10-03 18:05:04 +0530

    Updated build.sh

--- tests/build.sh
+++ tests/build.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+cd backend
+docker buildx build . --output type=docker,name=baserow_backend:latest | docker load
+cd ../web-frontend
+docker buildx build . --output type=docker,name=baserow_web-frontend:latest | docker load
+cd ../
 cp deploy/all-in-one/* ./
 rm -f docker-compose.yml
 mv docker-compose-new.yml docker-compose.yml
