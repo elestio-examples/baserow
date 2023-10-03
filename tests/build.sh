012f2ede8fbbd02347667da4370327e9428ae726
Prem Chavhan <pchavhan@logiquad.com>
2023-10-03 12:48:41 +0530

    Updated fixes

--- tests/build.sh
+++ tests/build.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 cp deploy/all-in-one/* ./
+rm -f docker-compose.yml
+mv docker-compose-new.yml docker-compose.yml
 docker buildx build . --output type=docker,name=elestio4test/baserow:latest | docker load
