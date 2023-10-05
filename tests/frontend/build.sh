9990536900846b216ecb8975fd36f20159181c68
Prem chavhan <pchavhan@logiquad.com>
2023-10-05 10:25:15 +0530

    fixes

--- tests/frontend/build.sh
+++ tests/frontend/build.sh
@@ -3,4 +3,5 @@ cp web-frontend/* ./
 rm -f docker-compose.yml
 mv docker-compose-new.yml docker-compose.yml
 sed -i "s~RUN yarn install~RUN yarn install --network-timeout 300000~g" Dockerfile
+sed -i "s~yarn install --production~yarn install --production --network-timeout 300000~g" Dockerfile
 docker buildx build . --output type=docker,name=elestio4test/baserow-frontend:latest | docker load
