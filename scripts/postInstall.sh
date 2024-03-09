366ab8edfabf0d34927821bdfe52e325e997def1
Joseph Benguira <z51biz@gmail.com>
2024-03-09 20:23:02 +0200

    Update postInstall.sh

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -3,7 +3,7 @@ set -o allexport; source .env; set +o allexport;
 
 #wait until the server is ready
 echo "Waiting for software to be ready ..."
-sleep 60s;
+sleep 120s;
 
 
   docker-compose exec -T baserow curl -X POST -H 'Content-Type: application/json' -i http://localhost/api/user/ --data '{ "name": "Admin", "email": "'${ADMIN_EMAIL}'", "password": "'${ADMIN_PASSWORD}'" }'
