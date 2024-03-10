fc885b1140473680cfa8751d63bec641a8e38edf
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-03-10 15:28:39 +0200

    fix

--- tests/allinone/build.sh
+++ tests/allinone/build.sh
@@ -1,18 +1,4 @@
 #!/usr/bin/env bash
-
-cp backend/Dockerfile ./Dockerfile
-docker buildx build . --platform linux/arm64,linux/amd64 --tag elestio4test/baserow-backend:latest
-docker buildx build . --platform linux/arm64,linux/amd64 --tag elestio4test/baserow-backend:latest --output type=docker | docker load
-
-# docker buildx build . --platform linux/arm64,linux/amd64 --output type=docker,name=elestio4test/baserow-backend:latest | docker load
-
-rm ./Dockerfile
-cp -r web-frontend/Dockerfile ./Dockerfile
-docker buildx build . --platform linux/arm64,linux/amd64 --tag elestio4test/baserow-frontend:latest
-docker buildx build . --platform linux/arm64,linux/amd64 --tag elestio4test/baserow-frontend:latest --output type=docker | docker load
-# docker buildx build . --platform linux/arm64,linux/amd64 --output type=docker,name=elestio4test/baserow-frontend:latest | docker load
-rm ./Dockerfile
-
 cp -r deploy/all-in-one/Dockerfile ./Dockerfile
 rm -f docker-compose.yml
 mv docker-compose-new.yml docker-compose.yml
