262295cf433a81f1978ef7325f090d0f67febda8
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-03-10 13:35:20 +0200

    fix

--- tests/allinone/build.sh
+++ tests/allinone/build.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
 cp backend/Dockerfile ./Dockerfile
-echo "Building backend..."
-docker buildx build . --platform linux/arm64,linux/amd64 --output type=docker,name=elestio4test/baserow-backend:latest | docker load
+docker buildx build . --platform linux/arm64,linux/amd64 --tag elestio4test/baserow-backend:latest
+docker buildx build . --platform linux/arm64,linux/amd64 --tag elestio4test/baserow-backend:latest --output type=docker | docker load
+
+# docker buildx build . --platform linux/arm64,linux/amd64 --output type=docker,name=elestio4test/baserow-backend:latest | docker load
 
 rm ./Dockerfile
 cp -r web-frontend/Dockerfile ./Dockerfile
-echo "Building frontend..."
-docker buildx build . --platform linux/arm64,linux/amd64 --output type=docker,name=elestio4test/baserow-frontend:latest | docker load
+docker buildx build . --platform linux/arm64,linux/amd64 --tag elestio4test/baserow-frontend:latest
+docker buildx build . --platform linux/arm64,linux/amd64 --tag elestio4test/baserow-frontend:latest --output type=docker | docker load
+# docker buildx build . --platform linux/arm64,linux/amd64 --output type=docker,name=elestio4test/baserow-frontend:latest | docker load
 rm ./Dockerfile
 
 cp -r deploy/all-in-one/Dockerfile ./Dockerfile
@@ -17,5 +20,4 @@ rm -f README.md
 mv README-new.md README.md
 sed -i "s~ARG FROM_WEBFRONTEND_IMAGE=baserow_web-frontend~ARG FROM_WEBFRONTEND_IMAGE=elestio4test/baserow-frontend:latest~g" ./Dockerfile
 sed -i "s~ARG FROM_BACKEND_IMAGE=baserow_backend~ARG FROM_BACKEND_IMAGE=elestio4test/baserow-backend:latest~g" ./Dockerfile
-echo "Building allinone..."
-docker buildx build . --platform linux/arm64,linux/amd64 --output type=docker,name=elestio4test/baserow-allinone:latest | docker load
+docker buildx build . --output type=docker,name=elestio4test/baserow-allinone:latest | docker load
