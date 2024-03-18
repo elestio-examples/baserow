330c77aa7e5e5066d72231af6718134add7bfb16
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-03-18 11:57:28 +0200

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -6,6 +6,13 @@ echo "Waiting for software to be ready ..."
 sleep 120s;
 
 
-  docker-compose exec -T baserow curl -X POST -H 'Content-Type: application/json' -i https://${DOMAIN}/api/user/ --data '{ "name": "Admin", "email": "'${ADMIN_EMAIL}'", "password": "'${ADMIN_PASSWORD}'" }'
+  # docker-compose exec -T baserow curl -X POST -H 'Content-Type: application/json' -i https://${DOMAIN}/api/user/ --data '{ "name": "Admin", "email": "'${ADMIN_EMAIL}'", "password": "'${ADMIN_PASSWORD}'" }'
 
-
+curl https://${DOMAIN}/api/user/ \
+  -H 'accept: application/json' \
+  -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6,zh-CN;q=0.5,zh;q=0.4,ja;q=0.3' \
+  -H 'cache-control: no-cache' \
+  -H 'content-type: application/json' \
+  -H 'pragma: no-cache' \
+  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36' \
+  --data-raw '{"name":"Admin","email":"'${ADMIN_EMAIL}'","password":"'${ADMIN_PASSWORD}'","authenticate":true,"language":"fr"}'
